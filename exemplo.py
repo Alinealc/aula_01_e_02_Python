d499bcfa4154a32f2b9cca715817c998565f5f90
Alinealc <alinea.alcantara@outlook.com>
2025-11-18 20:30:23 -0300

    Inicie o repositorio da Aula 01 e 02

---
+++ exemplo.py
@@ -0,0 +1,79 @@
+# nome = 'ANA'
+# idade = 25
+# salario = 15000.0
+# #saida
+# print(f'o nome dela é {nome} \nidade {idade} e o \nsalário {salario}')
+
+
+# x=5
+# x = '10'
+# print(x)
+
+
+# nome = input('Digite o nome: ')
+# print(f'Olá,{nome}!!')
+
+# cor = input('Digite a sua cor favorita: ')
+# print(f'A cor favorita é {cor}')
+
+
+# n1=5
+# n2=3
+# resultado = n1+n2
+# print(f'o resultado foi: {resultado}')    
+
+# n1 =float(input('Digite o primeiro valor: '))
+# n2 =float(input ('Digite o segundo valor: '))
+# resultado = n1*n2
+# print(f'o resultado é {resultado}')
+
+# n1=input('Digite a primeira nota:  ')
+# n2=input('Digite a segunda nota: ')
+# resultado = n1+n2//2 
+# print(f'a nota final é {resultado}')
+
+# nome_usuario = input('Informe seu nome: ')
+# idade_usuario = input('Informe sua idade: ')
+# print(f'O nome é {nome_usuario} e a sua idade é {idade_usuario}')
+
+# residencia = input('Olá informe qual o bairro e a cidade onde você mora ')
+# print(f'O local onde você reside é {residencia}:  ')
+
+
+# num1=float(input('Olá informe o primeiro número:  '))
+# num2=float(input('Informe o segundo número: '))
+# resultado = num1 * num2
+# print(resultado)
+
+
+# idade = int(input('Olá informe a sua idade: '))
+# print(f'sua idade é {idade}')  
+# ano= 2025
+# ano_nascimento = ano - idade
+# print(f'seu ano de nascimento é {ano_nascimento}: ')
+
+# idade = int(input('Olá informe a sua idade: '))
+# ano = 2025
+# ano_nascimento = ano - idade
+# print(f'seu ano de nascimento é {ano_nascimento}:  ')
+
+# saldo = float(input('Olá informe o valor do seu saldo: '))
+# ingresso = 250.00
+# ingresso_quantidade = saldo // ingresso
+# print(f'você pode comprar {ingresso_quantidade}')
+
+# preco = float(input(f'Informe o preço: '))
+# qtd = int(input('Informe a quantidade: '))
+# total = preco * qtd
+# desconto = total *0.1
+# valor_final = total - desconto
+# print(f'valor total {total}\nDesconto de 10% {desconto:.2f}\nValor a pagar {valor_final}')
+
+salario = float(input(f'Informe o seu salário: '))
+acrescimo = salario * 0.15
+salario_acrescimo = salario + acrescimo
+print(f'salario líquido {salario}\nnovo salario {salario_acrescimo:.2f}')
+
+
+
+
